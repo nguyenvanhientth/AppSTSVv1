d98f89de0c2985aad07777a41a874e56df73fabd
Hiền Nguyễn <nguyenvanhien200895@gmail.com>
2019-04-03 11:19:00 +0700

    update header

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -7,17 +7,18 @@ export default class HeaderComponent extends Component {
   render() {
     return (
       <View style={{
-        height: 40,
+        height: 70,
         flexDirection: 'row',
         justifyContent: 'flex-start',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: 'blue'
         }}
       >
         <TouchableHighlight
           style={{ marginLeft: 7, marginTop: 7 }}
           onPress={() => this.props.navigation.openDrawer()}>
           <Image
-            style={{ width: 40, height: 40 }}
+            style={{ width: 40, height: 40, marginTop: 20 }}
             source={require('../Icons/menuIcon.png')}
           />
         </TouchableHighlight>
