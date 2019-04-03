ff18e296e2cf54ae64d4129f15f11fee6d779c83
Hiền Nguyễn <nguyenvanhien200895@gmail.com>
2019-04-03 10:57:41 +0700

    Revert "Revert "Revert "Revert "test source tree""""
    This reverts commit 6e458ac0865c760f64e135d401a071cda932978e.

--- src/Screens/ProfileScreen.js
+++ src/Screens/ProfileScreen.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Text, View, ScrollView, StyleSheet, Platform } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Platform, Image } from 'react-native';
+
+const profile = require('../Images/profile.png');
 
 export default class MainScreen extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
     render(){
         return(
             <View style={styles.container}>
-            
+                <View style={styles.conProfile}>
+                    <Image source = {profile} style = {styles.imageProfile} />
+                </View>
                 <Text style={styles.text}>
                     Profile !
                 </Text>
@@ -18,10 +28,23 @@ const styles = StyleSheet.create({
     container: {
         flex : 1,
         backgroundColor : '#fff',
-        justifyContent : 'center',
+        //justifyContent : 'center',
         alignItems : 'center',
     },
     text: {
         fontSize : 30
+    },
+    conProfile: {
+        height: '50%',
+        width:'100%',
+        flex: 0.5,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#81F7F3'
+    },
+    imageProfile: {
+        height: '55%',
+        width: '55%'
     }
 })
